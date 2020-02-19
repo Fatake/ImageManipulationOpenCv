df6b10036fbb3088d3596e050255de7bb075fcbe
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-19 14:29:57 -0600

    Cambios en Translacion.py

--- translacion.py
+++ translacion.py
@@ -11,20 +11,12 @@ args = vars(ap.parse_args())
 imagen = cv2.imread(args["imagen"])
 cv2.imshow("Original",imagen)
 
-# Matriz M que define cuantos pixeles de desplazamiento
-# [1,0,tx] donde tx es el numero de pixeles que se movera a la izquierda o derecha
-# negativos izquierda
-# positivos derecha
-#
-# [0,1,ty] donde tx es num de piceles que se movera arriba o abajo
-# negativos arriba
-# positivos Abajo
-M = np.float32([[1, 0, 25], [0, 1, 50]])
-desplazo = cv2.warpAffine(imagen, M, (imagen.shape[1], imagen.shape[0]))
+
+desplazo = imutils.imageTranslate(imagen, 0, 100)
 cv2.imshow("Imagen desplazada abajo derecha",desplazo)
 
 M = np.float32([[1, 0, -50], [0, 1, -90]])
-desplazo = cv2.warpAffine(imagen, M, (imagen.shape[1], imagen.shape[0]))
+desplazo = imutils.imageTranslate(imagen, -50, -90)
 cv2.imshow("Imagen Desplazada arriba izquierda", desplazo)
 
 #Fin de archivo
