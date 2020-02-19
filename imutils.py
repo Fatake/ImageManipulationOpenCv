052f00b77b6ffbd510d0ca44911712cc9ff6c0e7
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-19 14:55:16 -0600

    Agregado reescala

--- imutils.py
+++ imutils.py
@@ -32,3 +32,22 @@ def rotate(imagen, angulo, center = None, scale = 1.0):
     rotada = cv2.warpAffine(imagen, M, (w, h))
 
     return rotada
+
+#
+# Funcion que reescala una imagen
+#
+def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height /float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width /float(w)
+        dim = (width,int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    return resized
