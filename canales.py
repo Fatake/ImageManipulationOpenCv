c075e3927fcfd4f1f6d05955a935155cd77742a7
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-26 12:09:20 -0600

    Canales de color

--- canales.py
+++ canales.py
@@ -19,3 +19,11 @@ cv2.imshow("Merged",merged)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+zeros = np.zeros(imagen.shape[:2], dtype ="uint8")
+cv2.imshow("Red", cv2.merge([zeros, zeros, R]))
+cv2.imshow("Green", cv2.merge([zeros, G, zeros]))
+cv2.imshow("Blue", cv2.merge([B, zeros, zeros]))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
