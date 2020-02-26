dd7d9a6d20d52c31b4d70f3a9d7a4e7a5aabfbbd
Fatake Raicos <pace_lozano@hotmail.com>
2020-02-26 12:56:43 -0600

    Histogramas 2D de colors

--- colorHistogrmas.py
+++ colorHistogrmas.py
@@ -24,6 +24,35 @@ for (canal,color) in zip(canales,colores):
     plt.plot(histograma, color=color)
     plt.xlim([0,256])
 
+fig = plt.figure()
+
+ax = fig.add_subplot(131)
+histograma = cv2.calcHist([canales[1], canales[0]], [0,1], None,
+        [32,32], [0,256,0,256])
+
+p = ax.imshow(histograma, interpolation = "nearest")
+ax.set_title("2D Hist G y B")
+plt.colorbar(p)
+
+ax = fig.add_subplot(132)
+hist = cv2.calcHist([canales[1], canales[2]], [0, 1], None,
+        [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation ="nearest")
+ax.set_title("2D Hist G y R")
+plt.colorbar(p)
+
+ax = fig.add_subplot(133)
+hist = cv2.calcHist([canales[0], canales[2]], [0, 1], None,
+        [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation ="nearest")
+ax.set_title("2D Hist B y R")
+plt.colorbar(p)
+
+#Histograma 3D
+hist = cv2.calcHist([imagen], [0, 1, 2],None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+print("3D histogram shape:{}, with{} values".format(hist.shape, hist.flatten().shape[0]))
+
+
 plt.show()
 
 cv2.waitKey(0)
